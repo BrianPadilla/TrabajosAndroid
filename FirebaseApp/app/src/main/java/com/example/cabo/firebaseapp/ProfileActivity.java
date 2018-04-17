2d594438f077a8dc0e2e68cc90d12a5dde6e4b61
CaboPrivate <cooler_mario@hotmail.com>
2018-04-16 23:26:00 -0600

    FireBase App

---
+++ FirebaseApp/app/src/main/java/com/example/cabo/firebaseapp/ProfileActivity.java
@@ -0,0 +1,145 @@
+package com.example.cabo.firebaseapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private static final int CHOOSE_IMAGE = 101;
+
+    TextView textView;
+    ImageView imageView;
+    EditText editText;
+
+    Uri uriProfileImage;
+    ProgressBar progressBar;
+
+    String profileImageUrl;
+
+    FirebaseAuth mAuth;
+
+    DatabaseReference databaseReference;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    public EditText nombre;
+    public EditText nombre2;
+    public EditText ncontrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
+
+
+
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        nombre2 = findViewById(R.id.txtnombre);
+        ncontrl = findViewById(R.id.txtcontrol);
+
+
+
+
+
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveUserInformation();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+
+
+
+    private void saveUserInformation() {
+
+        String nombre = nombre2.getText().toString().trim();
+        String control = ncontrl.getText().toString().trim();
+
+        UserInformation userInformation = new UserInformation(nombre,control);
+        FirebaseUser user = mAuth.getCurrentUser();
+        databaseReference.child("Alumnos").setValue(userInformation);
+        Toast.makeText(this,"guardado",Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuLogout:
+
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+
+                break;
+        }
+
+        return true;
+    }
+
+
+
+}
---
+++ FirebaseApp/app/src/main/java/com/example/cabo/firebaseapp/UserInformation.java
@@ -0,0 +1,15 @@
+package com.example.cabo.firebaseapp;
+
+/**
+ * Created by Cabo on 15/04/2018.
+ */
+
+public class UserInformation {
+    public String nombre;
+    public String ncontrol;
+
+    public UserInformation(String nombre,String ncontrol){
+        this.nombre=nombre;
+        this.ncontrol=ncontrol;
+    }
+}
